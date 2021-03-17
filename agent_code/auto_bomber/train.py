90ff0f4b17dcbb494e4d0252046142b986890fa0
Sophia Hammes <sophia.b.hammes@gmail.com>
2021-03-17 23:18:17 +0100

    intermediate stand

--- agent_code/auto_bomber/train.py
+++ agent_code/auto_bomber/train.py
@@ -3,7 +3,7 @@ from typing import List
 
 import events as e
 from agent_code.auto_bomber.callbacks import state_to_features
-
+import custom_events as ce
 # This is only an example!
 from agent_code.auto_bomber.transitions import Transitions
 
@@ -18,7 +18,6 @@ def setup_training(self):
     """
     # Example: Setup an array that will note transition tuples
     self.transitions = Transitions(state_to_features)
-
 
 def game_events_occurred(self, old_game_state: dict, last_action: str, new_game_state: dict, events: List[str]):
     """
@@ -40,9 +39,19 @@ def game_events_occurred(self, old_game_state: dict, last_action: str, new_game_
     """
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
 
+    # TODO: Punishment for up&down and left&right, remove WAIT
+    if last_action == 'MOVED_DOWN':
+        if 'MOVED_UP' in self.transitions.actions[-3:]:
+            events.append(ce.UP_AND_DOWN)
+    if last_action == 'MOVED_UP':
+        if 'MOVED_DOWN' in self.transitions.actions[-3:]:
+            events.append(ce.UP_AND_DOWN)
+
     # state_to_features is defined in callbacks.py
     self.transitions.add_transition(old_game_state, last_action, new_game_state, reward_from_events(self, events))
 
+
+    # TODO: Punishment, if agent is still in the same radius after certain time steps
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
     """
@@ -75,7 +84,6 @@ def reward_from_events(self, events: List[str]) -> int:
     Here you can modify the rewards your agent get so as to en/discourage
     certain behavior.
     """
-    # todo reward definition (right now only first sketch):
     # q: how to determine the winner?
     game_rewards = {
         e.COIN_COLLECTED: 50,
@@ -83,7 +91,9 @@ def reward_from_events(self, events: List[str]) -> int:
         e.INVALID_ACTION: -1,
         e.KILLED_SELF: -100,
         e.GOT_KILLED: -75,
-        e.SURVIVED_ROUND: 1000
+        e.SURVIVED_ROUND: 1000,
+        ce.UP_AND_DOWN: -50,
+        ce.LEFT_AND_RIGHT: -50
     }
     reward_sum = 0
     for event in events:
