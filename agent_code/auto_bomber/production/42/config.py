569af526cc7470dd59f90dcae727d9ad684e9f7f
Fabrizio Primerano <fabri.96p@gmail.com>
2021-03-28 14:14:32 +0200

    Cleaning the features file

--- agent_code/auto_bomber/production/42/config.py
+++
@@ -1,7 +0,0 @@
-MODELS_ROOT = "./models"
-# MODEL_DIR = "./models/41"
-MODEL_DIR = None
-ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
-EPSILON = 0.2
-DISCOUNT = 0.4
-LEARNING_RATE = 0.0003
