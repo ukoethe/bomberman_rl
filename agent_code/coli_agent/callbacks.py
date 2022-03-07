05db2aa156d5215c9065ba091b5cd18a774e4983
Ivo Schaper <ivoschaper@gmail.com>
2022-03-07 20:18:30 +0100

    Add logic for train vs. test case

--- agent_code/coli_agent/callbacks.py
+++ agent_code/coli_agent/callbacks.py
@@ -1,14 +1,45 @@
+import os
+
+import numpy as np
+
+from agent_code.tpl_agent.callbacks import state_to_features
+
+ACTIONS = ["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"]
+
+
 def setup(self):
-    """Sets up everything."""
-    self.model = DecisionTransformer()
-    return self.model
+    """Sets up everything. (First call)"""
+    if self.train or not os.path.isfile("q_table.npy"):
+        self.logger.info("Setting up Q-Learning algorithm")
+        self.number_of_states = 20  # TODO: make this dynamic
+        self.q_table = np.zeros(shape=(self.number_of_states, len(ACTIONS)))
+        self.exploration_rate_initial = 0.5
+        self.exploration_rate_end = 0.05  # at end of all episodes
+        self.exploration_decay_rate = 0.05  # 0.1 will reach min after ~ 100 episodes
+        # Finally this will call setup_training in train.py
 
-def act(self, game_state: dict):
-    pass
+    else:
+        self.logger.info("Loading learnt Q-Table")
+        self.q_table = np.load("q_table.npy")
 
+
+def act(self, game_state: dict) -> str:
+    """Takes in the current game state and returns the chosen action in form of a string."""
+    state = state_to_features(game_state)
+    if self.train and np.random.random() < self.exploration_rate:
+        self.logger.debug("Exploring")
+        return np.random.choice(ACTIONS)
+
+    self.logger.debug("Exploiting")
+    # TODO: Do we want to go 100% exploitation once we have learnt the q-table?
+    # Alternative is to sample from the learnt q_table distribution.
+    return ACTIONS[np.argmax(self.q_table[state])]
+
+
+# Only to demonstrate test
 class DecisionTransformer:
     def __init__(self):
         pass
 
     def adding_one(self, number: int) -> int:
-        return number+1
+        return number + 1
--- agent_code/coli_agent/train.py
+++ agent_code/coli_agent/train.py
@@ -1,10 +1,27 @@
+import numpy as np
+
+
 def setup_training(self):
-    """Sets up training
-    """
-    pass
+    """Sets up training"""
+    self.number_of_episodes = 100
+    self.exploration_rate = self.exploration_rate_initial
+
 
 def game_events_occurred(self, old_game_state, self_action, new_game_state, events):
-    pass
+    """Called once after each time step except the last. Used to collect training
+    data and filling the experience buffer.
+
+    Also, the actual learning takes place here.
+    """
+
 
 def end_of_round(self, last_game_state, last_action, events):
+    """Called once per agent (?) after the last step of a round."""
     pass
+
+
+def training_loop(self):
+    for episode in range(self.number_of_episodes):
+        self.exploration_rate = self.exploration_rate_end + (
+            self.exploration_rate_initial - self.exploration_rate_end
+        ) * np.exp(-self.exploration_decay_rate * episode)
