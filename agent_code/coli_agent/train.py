ad714e961ba266f3c8b64bf2b19886d3749f585e
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-08 02:33:30 +0100

    Copy and initially adapt reward_from_events()

--- agent_code/coli_agent/train.py
+++ agent_code/coli_agent/train.py
@@ -1,4 +1,8 @@
+from typing import List
+
 import numpy as np
+
+import events as e
 
 
 def setup_training(self):
@@ -18,6 +22,35 @@ def game_events_occurred(self, old_game_state, self_action, new_game_state, even
 def end_of_round(self, last_game_state, last_action, events):
     """Called once per agent (?) after the last step of a round."""
     pass
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    Returns a summed up reward/penalty for a given list of events that happened
+
+    Currently not assigning penalty to INVALID_ACTION because then it shouldn't even get this far (?).
+    Also not assigning reward/penalty to definitely(?) neutral actions MOVE LEFT/RIGHT/UP/DOWN or WAIT.
+    """
+    # TODO: customs events
+    # TODO: different rewards for different learning subtasks?
+    game_rewards = {
+        e.BOMB_DROPPED: 0.25,  # adjust aggressiveness
+        # e.BOMB_EXPLODED: 0,
+        e.COIN_COLLECTED: 1,
+        e.COIN_FOUND: 0.5,
+        # e.CRATE_DESTROYED: 0,  # possibly use if agent isn't destroying enough crates
+        e.GOT_KILLED: -5,  # adjust passivity
+        e.KILLED_OPPONENT: 5,
+        e.KILLED_SELF: -5,  # you dummy
+        # e.OPPONENT_ELIMINATED: 0,
+        e.SURVIVED_ROUND: 1,  # could possibly lead to not being active
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
 
 
 def training_loop(self):
