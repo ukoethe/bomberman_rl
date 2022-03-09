f77ef75e7c0afd89b2bc5140ec3bc200186c6282
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-09 14:11:20 +0100

    Add and comment out test for wall feature

--- agent_code/coli_agent/tests/test_callbacks.py
+++ agent_code/coli_agent/tests/test_callbacks.py
@@ -1,6 +1,17 @@
-from coli_agent.callbacks import DecisionTransformer
+from coli_agent.callbacks import DecisionTransformer, state_to_features
+
+# game_state_beginning = {
+#     # TODO
+# }
+
 
 def test_adding_one():
     dt = DecisionTransformer()
     result = dt.adding_one(5)
     assert result == 6
+
+
+# def test_wall_features(game_state_beginning):
+#     "Test if wall feature correctly counts walls"
+#     f = state_to_features(game_state_beginning)
+#     assert f[0] == 2
