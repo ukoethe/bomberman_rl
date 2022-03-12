8048739287d92a66a7791df7dd7dad8653c93a27
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-13 00:11:47 +0100

    Implemented some custom events

--- agent_code/coli_agent/train.py
+++ agent_code/coli_agent/train.py
@@ -18,7 +18,7 @@ TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
 WAS_BLOCKED = (
     "WAS_BLOCKED"  # tried to move into a wall/crate/enemy/explosion (strong penalty)
 )
-MOVED = "MOVED"  # moved somewhere and wasn't blocked (small reward)
+MOVED = "MOVED"  # moved somewhere (and wasn't blocked)
 
 PROGRESSED = "PROGRESSED"  # in last 5 turns, agent visited at least 3 unique tiles
 
@@ -82,20 +82,58 @@ def game_events_occurred(
     (if features = ... -> events.append(OUR_EVENT)). But events can also be added independently of features,
     just using game state in general. Leveraging of features more just to avoid code duplication.
     """
-    self.logger.debug(
-        f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}'
-    )
 
     # skip first timestep
     if old_game_state is None:
         return
 
+    old_state = state_to_features(self, old_game_state, self.history)
+    new_state = state_to_features(self, new_game_state, self.history)
+
+    with open("indexed_state_list.csv", encoding="utf-8", mode="r") as f:
+        state_list = f.readlines()
+        feature_vectors = []
+        for state in state_list:
+            cleaned_state = state.strip("\n").strip("[").strip("]")
+            feature_vectors.append(list(map(lambda x: int(x), cleaned_state.split())))
+
+    old_feature_vector = feature_vectors[old_state]
+    new_feature_vector = feature_vectors[new_state]
+
+    self.history[1].append(new_game_state["self"][-1])
+
+    # Custom events and stuff
+
+    if old_feature_vector[0] == 1 and new_feature_vector[0] == 0:
+        events.append(FLED)
+    elif old_feature_vector[0] == 0 and new_feature_vector[0] == 1:
+        events.append(SUICIDAL)
+    # possibly add the case when both is 1 (add new event for this so the penalty can be different)
+
+    if new_feature_vector[5] == 1:
+        events.append(PROGRESSED)
+
+    if new_game_state["self"][-1] != old_game_state["self"][-1]:
+        events.append(MOVED)
+
+    # if we want to have a wall counting reward, use this to add the event
+    # wall_counter = 0
+    # neighboring_coordinates = _get_neighboring_tiles(own_position, 1)
+    # for coord in neighboring_coordinates:
+    #     try:
+    #         if game_state["field"][coord] == -1:  # geht das? wer weiß
+    #             wall_counter += 1
+    #     except IndexError:
+    #         print(
+    #             "tried to access tile out of bounds (walls)"
+    #         )  # TODO remove, just for "debugging"
+
     # state_to_features is defined in callbacks.py
     self.transitions.append(
         Transition(
-            state_to_features(self, old_game_state, self.history),
+            old_state,
             self_action,
-            state_to_features(self, new_game_state, self.history),
+            new_state,
             reward_from_events(self, events),
         )
     )
@@ -119,17 +157,9 @@ def game_events_occurred(
     )
     self.logger.debug(f"Updated q-table: {self.q_table}")
 
-    # if we want to have a wall counting reward, use this to add the event
-    # wall_counter = 0
-    # neighboring_coordinates = _get_neighboring_tiles(own_position, 1)
-    # for coord in neighboring_coordinates:
-    #     try:
-    #         if game_state["field"][coord] == -1:  # geht das? wer weiß
-    #             wall_counter += 1
-    #     except IndexError:
-    #         print(
-    #             "tried to access tile out of bounds (walls)"
-    #         )  # TODO remove, just for "debugging"
+    self.logger.debug(
+        f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}'
+    )
 
 
 def end_of_round(self, last_game_state, last_action, events):
@@ -179,7 +209,7 @@ def reward_from_events(self, events: List[str]) -> int:
         e.KILLED_SELF: -10,  # you dummy --- this *also* triggers GOT_KILLED
         e.OPPONENT_ELIMINATED: 0.05,  # good because less danger or bad because other agent scored points?
         # e.SURVIVED_ROUND: 0,  # could possibly lead to not being active - actually penalize if agent too passive?
-        e.INVALID_ACTION: -1,  # necessary? (maybe for penalizing trying to move through walls/crates) - yes, seems to be necessary to learn that one cannot place a bomb after another placed bomb is still not exploded
+        e.INVALID_ACTION: -10,  # necessary? (maybe for penalizing trying to move through walls/crates) - yes, seems to be necessary to learn that one cannot place a bomb after another placed bomb is still not exploded
         WAS_BLOCKED: -20,
         MOVED: 0.5,
         PROGRESSED: 2,  # higher?
