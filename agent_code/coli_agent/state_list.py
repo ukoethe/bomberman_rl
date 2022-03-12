f7a523dc65b5a3e60b572428d58e198e873825b1
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-12 23:04:22 +0100

    Save indexed state list as np.array instead of list

--- agent_code/coli_agent/state_list.py
+++ agent_code/coli_agent/state_list.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 states = []
 binary = range(2)
 for a in binary:
@@ -5,7 +7,12 @@ for a in binary:
         for c in binary:
             for d in binary:
                 for e in binary:
-                    states.append([a, b, c, d, e])
+                    for f in binary:
+                        for g in range(3):
+                            for h in binary:
+                                states.append([a, b, c, d, e, f, g, h])
+
+states = np.array(states)
 
 with open("indexed_state_list.csv", encoding="utf-8", mode="w") as f:
     for i, state in enumerate(states):
