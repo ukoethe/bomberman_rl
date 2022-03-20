rotation bug

agent_code/agent_info/callbacks.py
@@ -25,6 +25,7 @@ def setup(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
+    # check if we are in training mode and if the model already exists
     if self.train or not os.path.isfile("model.pt"):
 
         self.logger.info("Training model.")
@@ -36,7 +37,6 @@ def setup(self):
             self.continue_train = False
 
         self.action_space_size = len(ACTIONS)
-        setup_training(self)
 
     else:
         self.logger.info("Loading model from saved state.")
@@ -54,18 +54,16 @@ def act(self, game_state: dict) -> str:
     :return: The action to take as a string.
     """
 
+    features = state_to_features(game_state)
+
     if self.train:
         action = train_act(self, game_state)
         return action
 
     self.logger.debug("Querying model for action")
-
-    features = state_to_features(game_state)
     action = self.model.choose_action(features)
-    np.where(ACTIONS == action)[0][0]
-    model_action = self.model.actions[action_index]
-    self.logger.debug("Model returnd action: ", model_action)
+    self.logger.debug("Model returnd action: ", action)
 
-    self.logger.debug("Querying model for action.")
-    return model_action
+
+    return action
 

agent_code/agent_info/model.py
@@ -9,12 +9,12 @@ class Q_Table:
         self.alpha = 0.1
         self.gamma = 0.9
         self.epsilon = 0.01
-        self.min_exploration = 0.6
-        self.exploration_decay = 0.0001
+        self.min_exploration = 0.3
+        self.exploration_decay = 1e-4
 
         self.game = game
 
-        self.actions = np.array(actions)
+        self.actions = actions
 
         self.q_table = defaultdict(
             lambda: np.zeros([game.action_space_size])
@@ -25,33 +25,36 @@ class Q_Table:
         action_index = np.argmax(self.q_table[features])
         return self.actions[action_index]
 
-    def update_q(self, old_game_state, new_game_state, self_action, rewards):
+    def update_q(self, batch):
 
-        actionIndex = np.where(self.actions == self_action)[0][0]
-        old_ft = state_to_features(old_game_state)
-        new_ft = state_to_features(new_game_state)
+        for sample in batch:
 
-        if type(old_ft) is not tuple:
-            self.game.logger.debug(f"{old_ft} was not a tuple")
-            return None
+            old_ft = sample[0]
+            action_index = sample[1]
+            new_ft = sample[2]
+            rewards = sample[3]
 
-        if type(new_ft) is not tuple:
-            self.game.logger.debug(f"{new_ft} was not a tuple")
-            return None
+            if type(old_ft) is not tuple:
+                self.game.logger.debug(f"{old_ft} was not a tuple")
+                return None
 
-        # We do not have to check if either of those exist
-        # If they dont default dict creates an np.zero array for us with that key
-        exspected_reward = self.q_table[old_ft][actionIndex]
-        max_next_reward = np.max(self.q_table[new_ft])
+            if type(new_ft) is not tuple:
+                self.game.logger.debug(f"{new_ft} was not a tuple")
+                return None
 
-        # The actual Q update step based on temporal difference
-        updated_q = (1 - self.alpha) * exspected_reward + self.alpha * (
-            rewards + self.gamma * max_next_reward
-        )
-        self.q_table[old_ft][actionIndex] = updated_q
+            # We do not have to check if either of those exist
+            # If they dont default dict creates an np.zero array for us with that key
+            exspected_reward = self.q_table[old_ft][action_index]
+            max_next_reward = np.max(self.q_table[new_ft])
 
-        if self.epsilon < 1 - self.min_exploration:
-            self.epsilon += self.exploration_decay
+            # The actual Q update step based on temporal difference
+            updated_q = (1 - self.alpha) * exspected_reward + self.alpha * (
+                rewards + self.gamma * max_next_reward
+            )
+            self.q_table[old_ft][action_index] = updated_q
+
+            if self.epsilon < 1 - self.min_exploration:
+                self.epsilon += self.exploration_decay
 
     def update_terminal(self, old_game_state, self_action, rewards):
 

agent_code/agent_info/train.py
@@ -1,5 +1,5 @@
 from collections import namedtuple, deque
-from copy import copy, deepcopy
+from random import sample
 import numpy as np
 from typing import List
 from agent_code.rule_based_agent.callbacks import act as rb_act, setup as rb_setup
@@ -15,7 +15,7 @@ import dill as pickle
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
 # Hyper parameters -- DO modify
-TRANSITION_HISTORY_SIZE = 5  # keep only ... last transitions
+TRANSITION_HISTORY_SIZE = 30  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
 
 # Events
@@ -39,6 +39,8 @@ def setup_training(self):
 
     # The 'model' in whatever form (NN, QT, MCT ...)
     self.model = Q_Table(self, ACTIONS)
+
+    self.batch_size = 10
 
     with open("model.pt", "wb") as file:
         pickle.dump(self.model, file)
@@ -89,12 +91,19 @@ def game_events_occurred(
     # Idea: Add your own events to hand out rewards
     if ...:
         events.append(PLACEHOLDER_EVENT)
-    rewards = reward_from_events(self, events)
 
-    self.model.update_q(old_game_state, new_game_state, self_action, rewards)
+    self.transitions.append(
+        Transition(
+            state_to_features(old_game_state),
+            np.where(self.actions == self_action)[0][0],
+            state_to_features(new_game_state),
+            reward_from_events(self, events),
+        )
+    )
 
-    # state_to_features is defined in callbacks.py
-    # self.transitions.append(Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward_from_events(self, events)))
+    if self.transitions > self.batch_size:
+        batch = sample(self.transitions, self.batch_size)
+        self.model.update_q(batch)
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
@@ -130,7 +139,13 @@ def reward_from_events(self, events: List[str]) -> int:
     game_rewards = {
         e.COIN_COLLECTED: 1,
         e.KILLED_OPPONENT: 5,
-        PLACEHOLDER_EVENT: -0.1,  # idea: the custom event is bad
+        e.WAITED: -1,
+        e.INVALID_ACTION: -10,
+        e.MOVED_LEFT: 0.1,
+        e.MOVED_RIGHT: 0.1,
+        e.MOVED_UP: 0.1,
+        e.MOVED_DOWN: 0.1,
+        # PLACEHOLDER_EVENT: -0.1,  # idea: the custom event is bad
     }
     reward_sum = 0
     for event in events:

agent_code/agent_info/utils.py
@@ -4,7 +4,7 @@ from random import shuffle
 from typing import Dict, List, Tuple
 
 # So we do not have to maintain this in multiple locations
-ACTIONS = ["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"]
+ACTIONS = np.array(["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"])
 
 
 def state_to_features(game_state: dict) -> np.array:
@@ -102,6 +102,7 @@ def relative_position(items: List[Tuple], agent: Tuple) -> List[Tuple]:
     This limits possible states without losing information
     """
     # TODO: Clean this up, the slight alteration item[0] is needed because bomb tuple is ((X,Y),Turns) and Coins just (X,Y)
+    # TODO: Only take in vision / target
     try:
         relative_position = [
             tuple(map(lambda i, j: i - j, item[0], agent)) for item in items
@@ -113,8 +114,9 @@ def relative_position(items: List[Tuple], agent: Tuple) -> List[Tuple]:
     return relative_position
 
 
-def rotation(game_state: Dict):
-
+def action_rotation(game_state: Dict):
+    # TODO: Figure out field rotation
+    # TODO: Or only rotate at start.
     """
     #IDEA not used for now
 
@@ -124,13 +126,20 @@ def rotation(game_state: Dict):
     global ACTIONS
 
     if game_state["self"][3] == (1, 1):
-        pass
+        # Ausgangspunkt
+        ACTIONS = np.array(["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"])
+
     if game_state["self"][3] == (15, 1):
-        pass
+        # Rechtsrotation
+        ACTIONS = np.array(["LEFT", "UP", "RIGHT", "DOWN", "WAIT", "BOMB"])
+
     if game_state["self"][3] == (1, 15):
-        pass
+        # Linksrotation
+        ACTIONS = np.array(["RIGHT", "DOWN", "LEFT", "UP", "WAIT", "BOMB"])
+
     if game_state["self"][3] == (15, 15):
-        pass
+        # 180 Grad
+        ACTIONS = np.array(["DOWN", "LEFT", "UP", "RIGHT", "WAIT", "BOMB"])
 
 
 def vision_field(game_state: Dict):