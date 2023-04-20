dd1236502b2c539c4ae1ff4d4f4536583b1daaef
Alberto Anzellotti <alberto.anzellotti@studenti.unitn.it>
2023-04-20 21:54:51 +0000

    documentation and waiter agent

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 # bomberman_rl
 Setup for a project/competition amongst students to train a winning Reinforcement Learning agent for the classic game Bomberman.
 
+
+# Callbacks file
+## this file must contain 2 mandatory functions
+`act` is called at every time step of the game:
+```
+def act(self, game_state):
+    ...
+    return action
+```
+where action is one of the following 6 strings
+`["UP", "DOWN", "LEFT", "RIGHT", "WAIT", "BOMB"] `
+
+
+
+`setup` is called only once at the beginning of the game, and **not** between multiple rounds of the same game:
+```
+def setup(self):
+    ...
+    return None
+```
+
+## game_state
+is the dict accessible by `act`, it contains the whole observable game state as described here:
+
+```
+        state = {
+            'round': self.round,
+            'step': self.step,
+            'field': np.array(self.arena),
+            'self': agent.get_state(),
+            'others': [other.get_state() for other in self.active_agents if other is not agent],
+            'bombs': [bomb.get_state() for bomb in self.bombs],
+            'coins': [coin.get_state() for coin in self.coins if coin.collectable],
+            'user_input': self.user_input,
+        }
+```
+
+| key               | **type** | **shape** | **values**                     | **meaning**                            |
+|-------------------|----------|-----------|--------------------------------|----------------------------------------|
+| **round**         | int      |           |                                |                                        |
+| **step**          | int      |           |                                |                                        |
+| **field**         | array    | [17,17]   | -1, 0, 1                       | wall , free , crate                    |
+| **self**          | array    | [4]       | [str,int,bool,(int,int)]       | [name, score, bombs_left, (x,y)]       |
+| **others**        | array    | [n]       | [[str,int,bool,(int,int)],...] | [[name, score, bombs_left, (x,y)],...] |
+| **bombs**         | array    | [n]       | [[int,int],...]                | [[x,y],...]                            |
+| **coins**         | array    | [n]       | [[int,int],...]                | [[x,y],...]                            |
+| **user_input**    | ?        | ?         | ?                              | ?                                      |
+| **explosion_map** | array    | [17,17]   | 0,1                            | safe,explosion                         |
+
+
+# Wall breaker
+## run a simulation
+To run a simulation the best approach is to use the command:
+```
+python main.py play --match-name "wall_breaker_t1" --save-replay --n-rounds 10 --my-agent wall_breaker
+```
+## agent
+
+The code of any agent is provided in a subfolder of `agent_code`.
+Such folder contains a subfolder `logs` , icons and the main script `callbacks.py`.
+
--- agent_code/wall_breaker/callbacks.py
+++ agent_code/wall_breaker/callbacks.py
@@ -13,7 +13,7 @@ import settings as s
 
 
 def look_for_targets(free_space, start, targets, logger=None):
-    """Find direction of closest target that can be reached via free tiles.
+    """Find direction of closest block.
 
     Performs a breadth-first search of the reachable free tiles until a target is encountered.
     If no target can be reached, the path that takes the agent closest to any target is chosen.
@@ -75,21 +75,23 @@ def setup(self):
     file for debugging (see https://docs.python.org/3.7/library/logging.html).
     """
     self.logger.debug("Successfully entered setup code")
+    self.current_round = 0
     np.random.seed()
     # Fixed length FIFO queues to avoid repeating the same actions
+    self.first_bomb = False
     self.bomb_history = deque([], 5)
-    self.coordinate_history = deque([], 20)
+    #self.coordinate_history = deque([], 20)
     # While this timer is positive, agent will not hunt/attack opponents
-    self.ignore_others_timer = 0
-    self.current_round = 0
+    #self.ignore_others_timer = 0
 
 
 def reset_self(self):
-    self.bomb_history = deque([], 5)
-    self.coordinate_history = deque([], 20)
+    #self.bomb_history = deque([], 5)
+    #self.first_bomb = False
+    #self.coordinate_history = deque([], 20)
     # While this timer is positive, agent will not hunt/attack opponents
-    self.ignore_others_timer = 0
-
+    #self.ignore_others_timer = 0
+    pass
 
 def act(self, game_state):
     """
@@ -99,6 +101,8 @@ def act(self, game_state):
     which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
     what it contains.
     """
+    #import pdb
+    #pdb.set_trace()
     self.logger.info("Picking action according to rule set")
     # Check if we are in a different round
     if game_state["round"] != self.current_round:
@@ -117,123 +121,5 @@ def act(self, game_state):
             if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
                 bomb_map[i, j] = min(bomb_map[i, j], t)
 
-    # If agent has been in the same location three times recently, it's a loop
-    if self.coordinate_history.count((x, y)) > 2:
-        self.ignore_others_timer = 5
-    else:
-        self.ignore_others_timer -= 1
-    self.coordinate_history.append((x, y))
 
-    # Check which moves make sense at all
-    directions = [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-    valid_tiles, valid_actions = [], []
-    for d in directions:
-        if (
-            (arena[d] == 0)
-            and (game_state["explosion_map"][d] < 1)
-            and (bomb_map[d] > 0)
-            and (not d in others)
-            and (not d in bomb_xys)
-        ):
-            valid_tiles.append(d)
-    if (x - 1, y) in valid_tiles:
-        valid_actions.append("LEFT")
-    if (x + 1, y) in valid_tiles:
-        valid_actions.append("RIGHT")
-    if (x, y - 1) in valid_tiles:
-        valid_actions.append("UP")
-    if (x, y + 1) in valid_tiles:
-        valid_actions.append("DOWN")
-    if (x, y) in valid_tiles:
-        valid_actions.append("WAIT")
-    # Disallow the BOMB action if agent dropped a bomb in the same spot recently
-    if (bombs_left > 0) and (x, y) not in self.bomb_history:
-        valid_actions.append("BOMB")
-    self.logger.debug(f"Valid actions: {valid_actions}")
-
-    # Collect basic action proposals in a queue
-    # Later on, the last added action that is also valid will be chosen
-    action_ideas = ["UP", "DOWN", "LEFT", "RIGHT"]
-    shuffle(action_ideas)
-
-    # Compile a list of 'targets' the agent should head towards
-    cols = range(1, arena.shape[0] - 1)
-    rows = range(1, arena.shape[0] - 1)
-    dead_ends = [
-        (x, y)
-        for x in cols
-        for y in rows
-        if (arena[x, y] == 0) and ([arena[x + 1, y], arena[x - 1, y], arena[x, y + 1], arena[x, y - 1]].count(0) == 1)
-    ]
-    crates = [(x, y) for x in cols for y in rows if (arena[x, y] == 1)]
-    targets = coins + dead_ends + crates
-    # Add other agents as targets if in hunting mode or no crates/coins left
-    if self.ignore_others_timer <= 0 or (len(crates) + len(coins) == 0):
-        targets.extend(others)
-
-    # Exclude targets that are currently occupied by a bomb
-    targets = [targets[i] for i in range(len(targets)) if targets[i] not in bomb_xys]
-
-    # Take a step towards the most immediately interesting target
-    free_space = arena == 0
-    if self.ignore_others_timer > 0:
-        for o in others:
-            free_space[o] = False
-    d = look_for_targets(free_space, (x, y), targets, self.logger)
-    if d == (x, y - 1):
-        action_ideas.append("UP")
-    if d == (x, y + 1):
-        action_ideas.append("DOWN")
-    if d == (x - 1, y):
-        action_ideas.append("LEFT")
-    if d == (x + 1, y):
-        action_ideas.append("RIGHT")
-    if d is None:
-        self.logger.debug("All targets gone, nothing to do anymore")
-        action_ideas.append("WAIT")
-
-    # Add proposal to drop a bomb if at dead end
-    if (x, y) in dead_ends:
-        action_ideas.append("BOMB")
-    # Add proposal to drop a bomb if touching an opponent
-    if len(others) > 0:
-        if (min(abs(xy[0] - x) + abs(xy[1] - y) for xy in others)) <= 1:
-            action_ideas.append("BOMB")
-    # Add proposal to drop a bomb if arrived at target and touching crate
-    if d == (x, y) and ([arena[x + 1, y], arena[x - 1, y], arena[x, y + 1], arena[x, y - 1]].count(1) > 0):
-        action_ideas.append("BOMB")
-
-    # Add proposal to run away from any nearby bomb about to blow
-    for (xb, yb), t in bombs:
-        if (xb == x) and (abs(yb - y) < 4):
-            # Run away
-            if yb > y:
-                action_ideas.append("UP")
-            if yb < y:
-                action_ideas.append("DOWN")
-            # If possible, turn a corner
-            action_ideas.append("LEFT")
-            action_ideas.append("RIGHT")
-        if (yb == y) and (abs(xb - x) < 4):
-            # Run away
-            if xb > x:
-                action_ideas.append("LEFT")
-            if xb < x:
-                action_ideas.append("RIGHT")
-            # If possible, turn a corner
-            action_ideas.append("UP")
-            action_ideas.append("DOWN")
-    # Try random direction if directly on top of a bomb
-    for (xb, yb), t in bombs:
-        if xb == x and yb == y:
-            action_ideas.extend(action_ideas[:4])
-
-    # Pick last action added to the proposals list that is also valid
-    while len(action_ideas) > 0:
-        a = action_ideas.pop()
-        if a in valid_actions:
-            # Keep track of chosen action for cycle detection
-            if a == "BOMB":
-                self.bomb_history.append((x, y))
-
-            return a
+    return "WAIT"
